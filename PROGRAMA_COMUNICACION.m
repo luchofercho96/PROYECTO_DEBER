6860f1071c54437d4b96f829acf1d242c22a97c5
luchofercho96 <49740356+luchofercho96@users.noreply.github.com>
2020-05-29 17:46:24 -0500

    matlab_bluce_comunicacion

--- PROGRAMA_COMUNICACION.m
+++ PROGRAMA_COMUNICACION.m
@@ -2,12 +2,25 @@
 clc,clear all,close all;
 loadlibrary('smClient64.dll','./smClient.h');
 %% ABRIR MEMORIA COMPARTIDA
-calllib('smClient64','openMemory','memoria_int',1);
-%% ESCRIBIR EN LA MEMORIA ABIERTA
-calllib('smClient64','setInt','memoria_int',1,56)
-%% LEER UN VALOR DE LA MEMEORIA
-lectura_1 = calllib('smClient64','getInt','memoria_int',1)
-lectura_0 = calllib('smClient64','getInt','memoria_int',0)
+calllib('smClient64','openMemory','memoria_int',2);
+%% TIEMPO DE SAMPLEO
+ts=0.1;
+%% CONDICIONES INICIALES
+t=0:ts:60;
+lectura_1(1)=0;
+%% senal de control iniical
+u(1)=0;
+kp=1;
+for k=1:length(t)
+    tic;
+    %% ESCRIBIR EN LA MEMORIA ABIERTA
+    calllib('smClient64','setFloat','memoria_int',1,u(k));
+    lectura_1(k) = calllib('smClient64','getFloat','memoria_int',1);
+    e(k)=lectura_1(k)-1;
+    u(k+1)=kp*e(k);
+    while(toc<ts)
+    end
+end
 %% LIBERAR MEMORIA COMPARTIDA
 calllib('smClient64','freeViews')
 unloadlibrary smClient64
