8fc0af94ca9329791266060d927ee83e9a375d15
luchofercho96 <49740356+luchofercho96@users.noreply.github.com>
2020-05-29 21:29:17 -0500

    matlab_bluce_comunicacion

--- PROGRAMA_COMUNICACION.m
+++ PROGRAMA_COMUNICACION.m
@@ -8,16 +8,20 @@ ts=0.1;
 %% CONDICIONES INICIALES
 t=0:ts:60;
 lectura_1(1)=0;
-%% senal de control iniical
+%% senal de control inical
 u(1)=0;
 kp=1;
 for k=1:length(t)
     tic;
     %% ESCRIBIR EN LA MEMORIA ABIERTA
-    calllib('smClient64','setFloat','memoria_int',1,u(k));
+    calllib('smClient64','setFloat','memoria_int',1,t(k)+rand(1));
+    calllib('smClient64','setFloat','memoria_int',0,1+t(k));
+    %% LECTURA EN LA MEMORIA ABIERTA
     lectura_1(k) = calllib('smClient64','getFloat','memoria_int',1);
+    %% LEY DE CONTROL
     e(k)=lectura_1(k)-1;
     u(k+1)=kp*e(k);
+    
     while(toc<ts)
     end
 end
